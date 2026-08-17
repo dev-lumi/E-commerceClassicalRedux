eaaa03db1a9fed642c0e0f1b799530983784a47e
dev-lumi <kishoreks1213@gmail.com>
2026-08-18 00:55:59 +0530

    axios fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-function App(){
-  return (
-    <div className="uppercase text-green-600">
-      <div> small change</div>
-      <div>another small change</div>
-      <div>another small change</div>
-      <div>another small change</div>
-      <div>another small change</div>
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-    </div>
-  )
+type Product = {
+  id: number;
+  title: string;
+  images: string[];
 }
 
-export default App
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+   async function fetchProducts() {
+    try{
+
+      const response = await axios.get("https://dummyjson.com/products");
+
+      setProducts(response.data.products);
+    }catch(error){
+      console.log(error)
+    }
+ }
+ fetchProducts()
+  }, []);
+  return (
+    <div className="uppercase italic text-[14px] font-semibold text-green-600 p-5 ">
+      {products.map((product,_id)=>(<div className="ring flex flex-col items-center p-2 my-5 rounded hover:bg-green-600 hover:text-white " key={product.id}><h1>{_id + 1}. {product.title}</h1>
+        <img className="w-40 h-40 " src={product.images[0]}/>
+      </div>))}
+    </div>
+  );
+}
+
+export default App;
